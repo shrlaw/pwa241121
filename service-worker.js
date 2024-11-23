56cfcf6b336a1ad87b2d7a527edb39a84fbf15de
Steve Rubin <shrlaw@comcast.net>
2024-11-23 03:13:14 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,24 @@
-const CACHE_NAME = 'robofriends-cache-v1';
+const CACHE_NAME = 'my-cache-v1'; // Give your cache a unique name
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/js/bundle.js',
-  '/static/js/main.chunk.js',
-  '/static/js/0.chunk.js',
-  '/static/css/main.css',
+  './',
+  './index.html',
+  './js/bundle.js',
+  './static/js/main.chunk.js',
+  './static/js/0.chunk.js',
+  './static/css/main.css',
 ];
 
-// Install event: Cache resources
+// Install event: Cache resources and log failed URLs
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+      return Promise.all(
+        urlsToCache.map((url) =>
+          cache.add(url).catch((error) => {
+            console.error(`Failed to cache: ${url}`, error);
+          })
+        )
+      );
     })
   );
 });
@@ -41,14 +47,58 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-caches.open('cache-name').then((cache) => {
-  return cache.addAll([
-      './index.html',
-      './static/js/main.js',
-      './static/css/main.css',
-      './favicon.ico',
-      './manifest.json',
-  ]).catch((error) => {
-      console.error('Caching failed for:', error);
-  });
-});
+
+// const CACHE_NAME = 'robofriends-cache-v1';
+// const urlsToCache = [
+//   '/',
+//   '/index.html',
+//   '/js/bundle.js',
+//   '/static/js/main.chunk.js',
+//   '/static/js/0.chunk.js',
+//   '/static/css/main.css',
+// ];
+
+// // Install event: Cache resources
+// self.addEventListener('install', (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       return cache.addAll(urlsToCache);
+//     })
+//   );
+// });
+
+// // Fetch event: Serve cached files when offline
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+
+// // Activate event: Clean up old caches
+// self.addEventListener('activate', (event) => {
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) => {
+//       return Promise.all(
+//         cacheNames.map((cacheName) => {
+//           if (cacheName !== CACHE_NAME) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
+
+// caches.open('cache-name').then((cache) => {
+//   return cache.addAll([
+//       './index.html',
+//       './static/js/main.js',
+//       './static/css/main.css',
+//       './favicon.ico',
+//       './manifest.json',
+//   ]).catch((error) => {
+//       console.error('Caching failed for:', error);
+//   });
+// });
